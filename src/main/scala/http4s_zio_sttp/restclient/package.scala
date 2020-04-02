cd9528537baeffdad4ff4e8188fb58fd0f67f0e9
Christoph Knabe <knabe@beuth-hochschule.de>
2020-04-02 16:19:00 +0200

    Weiterhin Compile-fehler, aber `RestClientService` hängt nur noch von `SttpClient` ab.

--- src/main/scala/http4s_zio_sttp/restclient/package.scala
+++ src/main/scala/http4s_zio_sttp/restclient/package.scala
@@ -5,20 +5,25 @@ import sttp.client.asynchttpclient.zio.SttpClient
 import sttp.model.Uri
 import zio.{Has, RIO, Task, ZIO}
 
+//Annotations according to https://zio.dev/docs/howto/howto_use_layers
 package object restclient {
 
+  //1. Define an object that gives the name to the module, this can be (not necessarily) a package object
   object RestClient {
+    //2. Within the module object define a trait Service that defines the interface our module is exposing,
+    // in our case 2 methods to retrieve and create a user
     /** The interface of a REST client for managing resources of type A */
     trait Service[A] {
-      def get(uri: Uri): ZIO[SttpClient with RestClient[A], Throwable, A]
+      def get(uri: Uri): ZIO[SttpClient, Throwable, A]
       def post(uri: Uri, a: A): ZIO[SttpClient, Throwable, Response[A]]
       def delete(id: Int): Task[Boolean]
     }
   }
 
-  type RestClient[A] = SttpClient with Has[RestClient.Service[A]]
+  //4. Define a type alias like: type ModuleName = Has[Service]
+  type RestClient[A] = Has[RestClient.Service[A]]
 
-  def getUser(uri: Uri): RIO[RestClient[User], User] = RIO.accessM(_.get[RestClient[User]].get(uri))
+  def getUser(uri: Uri): RIO[SttpClient with RestClient[User], User] = RIO.accessM(_.get[SttpClient].get(uri))
   def createUser(a: User): RIO[RestClient[User], User] = RIO.accessM(_.get.create(a))
   def deleteUser(id: Int): RIO[RestClient[User], Boolean] = RIO.accessM(_.get.delete(id))
 }
